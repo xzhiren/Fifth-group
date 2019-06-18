3e619377e3a93fa7e81d2eee6aadc368b8ec204f
xzhiren <xzhiren@year.net>
2019-06-18 22:47:31 +0800

    第五组项目整合版，由于能力有限，只能做简单整合

---
+++ JD/jd_homePage2/js/js.js
@@ -0,0 +1,18 @@
+/**
+ * Created by qq on 2019/6/16.
+ */
+var Nav = document.getElementById("Nav");
+console.log(Nav);
+window.onscroll = function(){
+    var scrollTop = document.documentElement.scrollTop;
+    if(scrollTop > 0){
+        Nav.className = "header_search nav_fix";
+    }else{
+        Nav.className = "header_search";
+    }
+}
+
+
+
+
+
